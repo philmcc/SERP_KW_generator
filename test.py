3a1b35b2ce6f0d537da99a7cd4baeb97301ffa89
philmcc <philmcc@gmail.com>
2022-06-07 17:37:05 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 
 
 import requests
-payload = {'api_key': '0cee3fe13dcac1e1086b04d623a89142', 'url':'https://www.google.com/search?client=firefox-b-d&q=gekko+codes', 'autoparse': 'true'}
+payload = {'api_key': '', 'url':'https://www.google.com/search?client=firefox-b-d&q=gekko+codes', 'autoparse': 'true'}
 r = requests.get('http://api.scraperapi.com', params=payload)
 print(r.text)
